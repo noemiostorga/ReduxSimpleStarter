4683b7b3a24f7b44edf7a66e06f5728990a09471
User <user@Users-MacBook-Pro.local>
2016-04-28 13:47:33 -0700

    added state

---
+++ src/components/search_bar.js
@@ -0,0 +1,38 @@
+// import React,{ Component } from 'react';
+
+
+// class SearchBar extends Component{
+// 	render(){
+// 		return <input onChange={this.onInputChange}/>
+// 	}
+// 	onInputChange(event){
+// 		console.log(event.target.value)
+// 		}
+// 	}
+
+
+// export default SearchBar;
+
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+constructor(props){
+	super(props)
+
+	this.state={term: ''};
+}	
+	render(){
+		return(
+				<div>
+					<input onChange={event => this.setState({term: event.target.value})} />;
+					value {this.state.term}
+				</div>
+
+			)
+	}
+}
+
+
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const App = function(){
-	return <div> Hi!</div>;
+
+const API_KEY = "AIzaSyCJOJyjR6J67ksSWipQvBE3zYz-MzqBoj4";
+
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
+
+
+const App = () =>{
+	return (
+	<div>
+		<SearchBar />
+	</div>
+	)
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
+
