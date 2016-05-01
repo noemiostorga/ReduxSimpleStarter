874775db69c27314510257cd7d4f39c097979f37
User <user@Users-MacBook-Pro.local>
2016-04-30 20:01:59 -0700

    refactored functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,21 +17,21 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component{
 constructor(props){
-	super(props)
+	super(props);
 
 	this.state={term: ''};
-}	
-	render(){
-		return(
-				<div>
-					<input onChange={event => this.setState({term: event.target.value})} />;
-					value {this.state.term}
-				</div>
-
-			)
-	}
 }
 
+	render(){
+		
+		return (
+			<div>
+				<input onChange={event => this.setState({term: event.target.value})}/>
+				value{this.state.term}
+			</div>
+		)
+	}
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
 
 
 const API_KEY = "AIzaSyCJOJyjR6J67ksSWipQvBE3zYz-MzqBoj4";
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-})
 
 
-const App = () =>{
+class App extends Component{
+	constructor(props){
+		super(props)
+
+		this.state= {videos: []}
+
+		YTSearch({key: API_KEY, term: "surfboard"},(videos) =>{
+			this.setState({videos});
+		})
+
+	}
+
+
+
+	render(){
+
 	return (
-	<div>
-		<SearchBar />
-	</div>
-	)
+			<div>
+				<SearchBar />
+			</div>
+		)
+	}
 }
-
 ReactDOM.render(<App />, document.querySelector(".container"));
 
