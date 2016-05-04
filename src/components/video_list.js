c0475f378ba4811bb6915c2c066dff8d02b78dac
User <user@Users-MacBook-Pro.local>
2016-05-03 19:44:45 -0700

    added loading spinner

---
+++ src/components/video_list.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+	const videoItem = props.videos.map((video)=>{
+		return( 
+
+		<VideoListItem 
+		onVideoSelect={props.onVideoSelect}
+		key={video.etag}
+		video={video} />
+		);
+	});
+
+	return(
+			<div className="col-md-4 list-group">
+				{videoItem}
+			</div>
+		);
+};
+
+export default VideoList;
